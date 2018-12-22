d614503660ef69f63d0c2bc1dc23300943a93aa3
Dawid Sobieszczuk <dawidsobieszczuk@gmail.com>
2018-12-22 20:27:45 +0100

    Create ECSObject.cs

---
+++ ECSObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ECS {
+    public abstract class ECSObject {
+        static protected List<Entity> _Entities;
+
+        public readonly long UID;
+        public string Name;
+
+        public ECSObject() {
+            UID = Helper.CreateUID();
+            Name = GetType().Name;
+        }
+    }
+}
